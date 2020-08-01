b1261719de3cd2eed024b216d4bb601825befa11
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-08-01 19:21:55 +0900

    8/1/2020

---
+++ Insetion_Sort.py
@@ -0,0 +1,10 @@
+array = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9]
+for i in range(0, 9):
+    j = i
+    while array[j] > array[j + 1]:
+        temp = array[j + 1]
+        array[j + 1] = array[j]
+        array[j] = temp
+        j= j - 1
+        print(array)
+    print(array)
---
+++ Selection_Sort.py
@@ -0,0 +1,19 @@
+# selection sort는 N*2 시간복잡도 입니다.
+# 효율성이 떨어진다.
+
+
+
+array = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9]
+for i in range(0, 10):
+    min = 9999
+    for j in range(i, 10):
+        if min > array[j]:
+            min = array[j]
+            index = j
+
+    temp = array[i]
+    array[i] = array[index]
+    array[index] = temp
+
+for i in range(0, 10):
+    print(array[i])
