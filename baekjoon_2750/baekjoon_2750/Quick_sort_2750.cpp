5f0f34a98dcb878eacfec3a447186cdc18fc6569
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-08-04 23:04:21 +0900

    8/4

---
+++ baekjoon_2750/baekjoon_2750/Quick_sort_2750.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int array[1001];
+
+int main(void) {
+
+	int number, i, j, min, index, temp;
+	scanf("%d", &number);
+
+	for (i = 0; i < number; i++) {//ют╥б
+		scanf("%d", &array[i]);
+	}
+
+	for (i = 0; i < number; i++) {
+		min = 1001;
+		for (j = i; i < number; j++) {
+			if (min > array[j]) {
+				min = array[j];
+				index = j;
+			}
+		}
+		temp = array[i];
+		array[i] = array[index];
+		array[index] = temp;
+	}
+
+	for (i = 0; i < number; i++) {
+		printf("%d\n", array[i]);
+	}
+}
